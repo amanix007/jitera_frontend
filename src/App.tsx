1f81317f3f104a922e09c2c777a164a80d520f6a
Aman Ullah <amanix007@gmail.com>
2021-12-10 19:43:15 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'antd/dist/antd.css';
+import './App.scss'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <p>potato</p>
+    </div>
+  )
+}
+
+export default App
